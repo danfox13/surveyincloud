11858741b34ea65c8188877442ab687449166643
Dan Fox <dan.fox@digital.hmrc.gov.uk>
2017-01-18 21:06:00 +0000

    Submitting form to db

--- submit.php
+++ submit.php
@@ -10,14 +10,32 @@ $db = substr($url["path"], 1);
 $conn = new mysqli($server, $username, $password, $db);
 
 
-echo $_POST["name"];
-echo $_POST["event"];
-echo $_POST["q1"];
-echo $_POST["q2"];
-echo $_POST["q3"];
-echo $_POST["q4"];
-echo $_POST["q5"];
-echo $_POST["comments"];
-echo "<br/>";
-echo $url["user"];
+$name = $_POST["name"];
+$event = $_POST["event"];
+$q1 = $_POST["q1"];
+$q2 = $_POST["q2"];
+$q3 = $_POST["q3"];
+$q4 = $_POST["q4"];
+$q5 = $_POST["q5"];
+$comments = $_POST["comments"];
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    //forward to error
+} 
+
+$sql = "INSERT INTO heroku_686d4942c2b2587.surveyresponse (name, event, q1, q2, q3, q4, q5, comments)
+VALUES ('${name}', '${event}', '${q1}', '${q2}', '${q3}', '${q4}', '${q5}', '${comments}'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    //forward to error
+}
+
+$conn->close();
+
+header('Location: complete.php');
 ?>
