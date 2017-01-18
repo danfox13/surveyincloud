83cc571bfc7001c5f2fb71b42f11099d239ee2bc
Dan Fox <dan.fox@digital.hmrc.gov.uk>
2017-01-18 20:23:21 +0000

    Adding db conn

--- submit.php
+++ submit.php
@@ -1,4 +1,15 @@
 <?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+
 echo $_POST["name"];
 echo $_POST["event"];
 echo $_POST["q1"];
@@ -6,6 +17,7 @@ echo $_POST["q2"];
 echo $_POST["q3"];
 echo $_POST["q4"];
 echo $_POST["q5"];
-echo $_POST["q5"];
 echo $_POST["comments"];
+echo "<br/>"
+echo $url["user"];
 ?>
