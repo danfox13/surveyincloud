f680a1277c784ff504888887e5699707db59cc58
Dan Fox <dan.fox@digital.hmrc.gov.uk>
2017-01-23 15:42:19 +0000

    SQL injection

--- submit.php
+++ submit.php
@@ -30,17 +30,34 @@ if ($conn->connect_error) {
     header('Location: error.php');
 } 
 
-$sql = "INSERT INTO heroku_686d4942c2b2587.surveyresponse (name, event, q1, q1Comment, q2, q2Comment, q3, q3Comment, q4, q4Comment, q5, q5Comment, comments)
-VALUES ('${name}', '${event}', '${q1}', '${q1Comment}', '${q2}', '${q2Comment}', '${q3}','${q3Comment}', '${q4}', '${q4Comment}', '${q5}', '${q5Comment}', '${comments}')";
+// $sql = "INSERT INTO heroku_686d4942c2b2587.surveyresponse (name, event, q1, q1Comment, q2, q2Comment, q3, q3Comment, q4, q4Comment, q5, q5Comment, comments)
+// VALUES ('${name}', '${event}', '${q1}', '${q1Comment}', '${q2}', '${q2Comment}', '${q3}','${q3Comment}', '${q4}', '${q4Comment}', '${q5}', '${q5Comment}', '${comments}')";
 
-if ($conn->query($sql) === TRUE) {
-    header('Location: complete.php');
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    //header('Location: error.php');
-}
+// if ($conn->query($sql) === TRUE) {
+//     header('Location: complete.php');
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+//     //header('Location: error.php');
+// }
+
+$stmt = $conn->prepare('INSERT INTO heroku_686d4942c2b2587.surveyresponse (name, event, q1, q1Comment, q2, q2Comment, q3, q3Comment, q4, q4Comment, q5, q5Comment, comments) VALUES (:name, :event, :q1, :q1Comment, :q2, :q2Comment, :q3, :q3Comment, :q4, :q4Comment, :q5, :q5Comment, :comments)');
+
+$stmt->execute(array(
+    "name" => $name,
+    "event" => $event
+    "q1" => $q1
+    "q1Comment" => $q1Comment,
+    "q2" => $q2,
+    "q2Comment" => $q2Comment,
+    "q3" => $q3,
+    "q3Comment" => $q3Comment,
+    "q4" => $q4,
+    "q4Comment" => $q4Comment,
+    "q5" => $q5,
+    "q5Comment" => $q5Comment,
+    "comments" => $comments));
 
 $conn->close();
-
+header('Location: complete.php');
 
 ?>
