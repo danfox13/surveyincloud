9d5aff084efdebfecdc3897134452e0fc2448a43
Dan Fox <dan.fox@digital.hmrc.gov.uk>
2017-01-24 23:21:53 +0000

    results

--- results.php
+++ results.php
@@ -28,7 +28,8 @@ if (!$conn) {
       google.charts.load('current', {'packages':['corechart']});
 
       // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawQ1, drawQ2);
+      google.charts.setOnLoadCallback(drawQ1);
+      google.charts.setOnLoadCallback(drawQ2);
 
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
